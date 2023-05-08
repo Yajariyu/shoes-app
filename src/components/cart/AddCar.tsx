08bf08d4dfbad9aa725103f5b35e7882452ee584
Yajariyu Cardoza <yajariyu@gmail.com>
2023-05-08 19:13:39 -0400

    style button

--- src/components/cart/AddCar.tsx
+++ src/components/cart/AddCar.tsx
@@ -20,7 +20,7 @@ const AddCart: FC<AddCartProps> = ({ handleAddCart }) => {
 
   return (
     <div className='bg-gray-200 rounded-md  w-[80%] md:min-w-[65%] py-3 px-4 fixed bottom-2 ' onClick={handleAdd}>
-      <button className='bg-purple-600 rounded-full px-2 py-2 text-white'>Add to Cart</button>
+      <button className='bg-purple-600 rounded-full px-2 py-2 text-white w-full md:w-fit'>Add to Cart</button>
     </div>
   )
 }
