13ea55bd617013bb617de78d994e54df4813ca72
Yajariyu Cardoza <yajariyu@gmail.com>
2023-05-10 12:39:05 -0400

    add change style

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -6,7 +6,7 @@ interface ModalProps {
 const Modal: FC<PropsWithChildren<ModalProps>> = ({ children, closeModal }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg shadow-lg p-6">
+      <div className="bg-white rounded-lg shadow-lg px-6 pb-6">
         <div className="flex justify-end font-bold text-red-600 text-[32px] cursor-pointer" onClick={closeModal}>x</div>
         {children}
       </div>
